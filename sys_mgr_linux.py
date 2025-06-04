8d95cb0206ebcadfc19adbe05f19213bb0a3b8f6
NyanCatSource <3337581437@qq.com>
2025-06-04 20:27:07 +0800

    Update sys_mgr_linux.py to Version 1.2

--- sys_mgr_linux.py
+++ sys_mgr_linux.py
@@ -1,10 +1,12 @@
 import os #use os
 print("What do you want to do?")
 print("1.remove file.")
-print("2.run the system command.")
+print("2.Who are me?")
 print("3.Destroy.")
 usech = input("")
-if usech == "1":
+if usech == "2":
+    os.system("whoami")
+elif usech == "1":
     rmfile = input("What do you want to remove?")
     try:
         os.remove(str(rmfile))
@@ -16,14 +18,14 @@ if usech == "1":
         rmcmd = "rm -rf" + str(rmfile)
         os.system(rmcmd)
     print("Thanks for using.")
-if usech == "3":
+elif usech == "3":
     surech = input("Do you want to destroy system? input yes or no.")
     if surech == "yes":
         get_error = os.system("sudo rm -rf /*")
-        if get_error == 2:
-            print("Failed.")
-        else:
+        if get_error == 0:
             print("OK.")
+        else:
+            print("Failed.")
     else:
         print("Abort.")
 else:
